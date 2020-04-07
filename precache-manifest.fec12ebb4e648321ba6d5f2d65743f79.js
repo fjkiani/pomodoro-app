4ec363cd9dc145a7c1ff6f1f02795b3eb768e129
fahad <fjkiani@icloud.com>
2020-04-07 17:20:01 -0400

    Updates

---
+++ precache-manifest.fec12ebb4e648321ba6d5f2d65743f79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e05712784c7acf26f21dd5b8c414231",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1143482a88c54ea12894",
+    "url": "/static/css/main.5f51cc8e.chunk.css"
+  },
+  {
+    "revision": "35174f16227eb4f78ebd",
+    "url": "/static/js/2.a43a2bd2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a43a2bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1143482a88c54ea12894",
+    "url": "/static/js/main.1d80d094.chunk.js"
+  },
+  {
+    "revision": "b3abab3d4dc7605a036c",
+    "url": "/static/js/runtime-main.2caa9e0e.js"
+  }
+]);
